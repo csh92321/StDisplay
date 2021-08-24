3c3d356c4128c4316159f16967a4ddbb87df4137
HongJunMo <wnsah717@daum.net>
2021-08-24 14:17:17 +0900

    main 작성

--- src/stdisplay/Stdisplay.java
+++ src/stdisplay/Stdisplay.java
@@ -1,8 +1,44 @@
 package stdisplay;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Stdisplay {
-	public void display() {
+	public static void main(String[] args) {
+		
+		ArrayList<StudentInfo> std= new ArrayList<>(); 
+		
+		Scanner scan =new Scanner(System.in);
+		System.out.println("==== 학생성적 관리 프로그램====");
+		System.out.println("1.성적 추가 2.성적 수정 3.성적 검색 ");
+		System.out.println("4.성적 삭제 5.모든 성적 확인");
+		int choice = scan.nextInt();
+		
+		switch(choice) {
+		case 1:
+			break;
+				//성적 추가
+		case 2:
+			break;
+			// 성적 수정
+		case 3:
+			break;
+			//성적 검색
+		case 4:
+			break;
+			// 성적 삭제
+		case 5:
+			//모든 성적 확인
+			break;
+		
+		}
+		
+		
+		
+		
+		
+		
 		
 	}
-
+	
 }
