d4bad4fe217d6f5b53049aad5413b540f206b2fd
HongJunMo <wnsah717@daum.net>
2021-08-24 14:35:15 +0900

    메인 수정 1

--- src/stdisplay/Stdisplay.java
+++ src/stdisplay/Stdisplay.java
@@ -9,26 +9,26 @@ public class Stdisplay {
 		ArrayList<StudentInfo> std= new ArrayList<>(); 
 		
 		Scanner scan =new Scanner(System.in);
-		System.out.println("==== 학생성적 관리 프로그램====");
-		System.out.println("1.성적 추가 2.성적 수정 3.성적 검색 ");
-		System.out.println("4.성적 삭제 5.모든 성적 확인");
+		System.out.println("==== 학생 관리 프로그램====");
+		System.out.println("1.학생 등록 2.학생 확인 3.학생 수정 ");
+		System.out.println("4.학생 삭제 5.성적 관리");
 		int choice = scan.nextInt();
 		
 		switch(choice) {
 		case 1:
 			break;
-				//성적 추가
+				//학생등록 / 주희님
 		case 2:
 			break;
-			// 성적 수정
+			// 학생 확인  / 청하님
 		case 3:
 			break;
-			//성적 검색
+			//학생 수정  / 성환님 
 		case 4:
 			break;
-			// 성적 삭제
+			// 학생 삭제
 		case 5:
-			//모든 성적 확인
+			// 성적 관리 / 소영님
 			break;
 		
 		}
