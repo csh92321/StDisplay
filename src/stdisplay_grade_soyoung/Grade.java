cad04248c6440361ac6a54e44e21f0f733d644ec
Choi.SY <sollly0829@gmail.com>
2021-08-25 14:32:05 +0900

    grade 수정중2 : 합쳐서 테스트

--- src/stdisplay_grade_soyoung/Grade.java
+++ src/stdisplay_grade_soyoung/Grade.java
@@ -31,8 +31,8 @@ public class Grade {
 				for(int i=0; i<sd.std.size();i++) {
 					String name = sd.std.get(i).getName();
 					if (name.equals(stName)) {
+						putGrade(i);
 						
-						putGrade();
 					} else {
 						System.out.println(stName+" 학생은 존재하지 않습니다");
 					}
@@ -58,11 +58,14 @@ public class Grade {
 				System.out.print("학생 이름 입력 : ");
 				stName = sc.next();
 				
-				if (si.getName().equals(stName)) {
-					System.out.println("수정할 과목 입력 (전과목 재입력시 '전과목'작성)");
-					System.out.print(">> ");
-					String subject = sc.next();
-					correctGrade(subject);
+				for(int i=0; i<sd.std.size();i++) {
+					String name = sd.std.get(i).getName();
+					if (name.equals(stName)) {
+						System.out.println("수정할 과목 입력 (전과목 재입력시 '전과목'작성)");
+						System.out.print(">> ");
+						String subject = sc.next();
+						correctGrade(subject,i);
+					}
 				}
 				break;
 			case 4 :
@@ -71,51 +74,51 @@ public class Grade {
 		}
 	}
 	
-	public void putGrade() {
+	public void putGrade(int num) {
 	
 		System.out.print("국어 성적 입력 : ");
 		int kor = sc.nextInt();
-		si.setKor(kor);
+		sd.std.get(num).setKor(kor);
 		System.out.print("영어 성적 입력 : ");
 		int eng = sc.nextInt();
-		si.setEng(eng);
+		sd.std.get(num).setEng(eng);
 		System.out.print("수학 성적 입력 : ");
 		int math = sc.nextInt();
-		si.setKor(math);
+		sd.std.get(num).setMath(math);
 
-		double avg = kor+eng+math /3.0;
-		si.setAvg(avg);
+		double avg = (kor+eng+math)/3.0;
+		sd.std.get(num).setAvg(avg);
 
-		sd.std.add(si);
+		
 	}
 	
 	public void checkGrade(int num) {
-		System.out.println("국어 : " + sd.std.get(num).getKor() + "영어 : " + sd.std.get(num).getEng() 
-		+ "수학 : " + sd.std.get(num).getMath() + "평균 : " + sd.std.get(num).getAge());
+		System.out.printf("국어 : %d 영어 : %d 수학 : %d 평균 : %.2f \n",sd.std.get(num).getKor(), sd.std.get(num).getEng() 
+		,sd.std.get(num).getMath(),sd.std.get(num).getAvg());
 	}
 	
-	public void correctGrade(String subject) {
+	public void correctGrade(String subject,int num) {
 		
 		if (subject.equals("국어")) {
 			System.out.print("국어 점수 재입력 : ");
 			int kor = sc.nextInt();
-			si.setKor(kor);
-			double avg = (si.getKor()+si.getEng()+si.getMath())/3.0;
-			si.setAvg(avg);
+			sd.std.get(num).setKor(kor);
+			double avg = (sd.std.get(num).getKor()+sd.std.get(num).getEng()+sd.std.get(num).getMath())/3.0;
+			sd.std.get(num).setAvg(avg);
 		} else if (subject.equals("수학")) {
 			System.out.print("수학 점수 재입력 : ");
 			int math = sc.nextInt();
-			si.setMath(math);
-			double avg = (si.getKor()+si.getEng()+si.getMath())/3.0;
-			si.setAvg(avg);
+			sd.std.get(num).setMath(math);
+			double avg = (sd.std.get(num).getKor()+sd.std.get(num).getEng()+sd.std.get(num).getMath())/3.0;
+			sd.std.get(num).setAvg(avg);
 		} else if (subject.equals("영어")) {
 			System.out.print("영어 점수 재입력 : ");
 			int eng = sc.nextInt();
-			si.setEng(eng);
-			double avg = (si.getKor()+si.getEng()+si.getMath())/3.0;
-			si.setAvg(avg);
+			sd.std.get(num).setEng(eng);
+			double avg = (sd.std.get(num).getKor()+sd.std.get(num).getEng()+sd.std.get(num).getMath())/3.0;
+			sd.std.get(num).setAvg(avg);
 		} else if (subject.equals("전과목")) {
-			putGrade();
+			putGrade(num);
 		} else {
 			System.out.println("잘못된 입력입니다");
 		}
