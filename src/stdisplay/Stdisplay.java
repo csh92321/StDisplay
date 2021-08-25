e67f636fa61b296ba226306b1e0e8fa83197cf87
Choi.SY <sollly0829@gmail.com>
2021-08-25 11:33:46 +0900

    1차 master->soyoung 머징

--- src/stdisplay/Stdisplay.java
+++ src/stdisplay/Stdisplay.java
@@ -13,37 +13,36 @@ public class Stdisplay {
 	public static void main(String[] args) {
 		
 		ArrayList<StudentInfo> std= new ArrayList<>(); 
-
 		Registration_joohui re = new Registration_joohui();
-
-		Grade grade = new Grade();
-
-		
+		Grade grade = new Grade();	
 		Scanner scan =new Scanner(System.in);
-		System.out.println("==== 학생 관리 프로그램====");
-		System.out.println("1.학생 등록 2.학생 확인 3.학생 수정 ");
-		System.out.println("4.학생 삭제 5.성적 관리");
-		int choice = scan.nextInt();
 		
-		switch(choice) {
-		case 1:
-			re.display();
-			break;
-				//학생등록 / 주희님
-		case 2:
-			break;
-			// 학생 확인  / 청하님
-		case 3:
-			break;
-			//학생 수정  / 성환님 
-		case 4:
-			break;
-			// 학생 삭제
-		case 5:
-			grade.display();
-			break;
-		
+		while(true) {
+			System.out.println("==== 학생 관리 프로그램====");
+			System.out.println("1.학생 등록 2.학생 확인 3.학생 수정 ");
+			System.out.println("4.학생 삭제 5.성적 관리");
+			int choice = scan.nextInt();
+			
+			switch(choice) {
+			case 1:
+				re.display();
+				break;
+					//학생등록 / 주희님
+			case 2:
+				break;
+				// 학생 확인  / 청하님
+			case 3:
+				break;
+				//학생 수정  / 성환님 
+			case 4:
+				break;
+				// 학생 삭제
+			case 5:
+				grade.display();
+				break;
+			}
 		}
+		
 		
 		
 		
