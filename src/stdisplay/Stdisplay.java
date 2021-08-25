cdb0a6898b8bd4ce5589c4d2e71fe06bb0df0bca
HongJunMo <wnsah717@daum.net>
2021-08-25 11:21:02 +0900

    삭제기능 구현

--- src/stdisplay/Stdisplay.java
+++ src/stdisplay/Stdisplay.java
@@ -5,23 +5,17 @@ import java.util.*;
 import stdisplay_delete_hjm.StdDelete;
 
 public class Stdisplay {
-	
-	
-	private static  ArrayList<StudentInfo> std = new ArrayList<>();
-	
-	public  ArrayList<StudentInfo> getaddr()
-	{
+
+	private static ArrayList<StudentInfo> std = new ArrayList<>();
+
+	public ArrayList<StudentInfo> getaddr() {
 		return std;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		StdDelete hjm = new StdDelete();
-			
-		Stdisplay sp = new Stdisplay();
-		ArrayList<StudentInfo> std = sp.getaddr();
-		
+
 
 		while (true) {
 			System.out.println("==== 학생 관리 프로그램====");
@@ -46,22 +40,13 @@ public class Stdisplay {
 				break;
 			// 학생 수정 / 성환님
 			case 4:
-				hjm.delete();
-				System.out.println("================");
-				hjm.stdSrc();
-				System.out.println("================");
 				hjm.print();
-				System.out.println("222================");
-				hjm.stdSrc();
-				System.out.println("333================");
 				break;
-
 			case 5:
 				// 성적 관리 / 소영님
 				break;
 
 			}
-
 		}
 
 	}
--- src/stdisplay_delete_hjm/StdDelete.java
+++ src/stdisplay_delete_hjm/StdDelete.java
@@ -1,39 +1,44 @@
 package stdisplay_delete_hjm;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import stdisplay.Stdisplay;
 import stdisplay.StudentInfo;
 
 public class StdDelete {
-
+	boolean flag = false;
 	Scanner scan = new Scanner(System.in);
 	StudentInfo st = new StudentInfo();
 	Stdisplay aa = new Stdisplay();
 
-	public void delete() {
-
-		for (int i = 0; i < 1; i++) {
-			StudentInfo st = new StudentInfo();
-			System.out.println(">>>");
-			String name = scan.next();
-			st.setName(name);
-			aa.getaddr().add(st);
+	public void check() {
+		System.out.print("이름 입력 :");
+		String name = scan.next();
+		for (int i = 0; i < aa.getaddr().size(); i++) {
+			StudentInfo d = aa.getaddr().get(i);
+			if (name.equals(d.getName())) {
+				aa.getaddr().remove(i);
+				System.out.println("삭제완료");
+				flag = true;
+				break;
+			} else {
+				flag = false;
+			}
 		}
 	}
 
-	public void stdSrc() {
-		for (int i = 0; i < aa.getaddr().size(); i++) {
-		StudentInfo d = aa.getaddr().get(i);
-		System.out.println("#######:" + d.getName());
-	}
-		
-
+	public void a() {
+		if (flag != true) {
+			System.out.println("없는 학생 정보");
+		}
 	}
 
 	public void print() {
-				aa.getaddr().remove(0);
 
+		System.out.println("==== 학생 삭제 ====");
+		System.out.println("학생 이름 입력 ");
+		System.out.print(">>>");
+		check();
+		a();
 	}
 }
